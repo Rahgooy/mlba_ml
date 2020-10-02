d175dab8c9fef9fde8473973d516415c94ac42d2
taher <taher.rahgooy@gmail.com>
2020-10-01 19:14:32 -0500

    minor changes

--- lba_dist.py
+++ lba_dist.py
@@ -58,7 +58,7 @@ class LBA:
         p += t*s / A * npdf((b - A - t*d) / (t*s))
         p -= t*s / A * npdf((b - t*d) / (t*s))
 
-        return p.clamp(0)
+        return p
 
     def timePDF(self, t):
         A, b, d, s = self.A, self.b, self.d, self.s
@@ -87,7 +87,7 @@ class LBA:
         for i in range(self.d.shape[0]):
             # np.ceil((self.b[0].item() - self.A[0].item()) / self.d[0, i].item())
             a = 0
-            b = np.ceil(self.b[0].item() / self.d[i].item()) * 3
+            b = np.ceil(self.b.item() / self.d[i].item()) * 3
             print(b)
             res[i] = simps(lambda x: self.firstTimePdf(
                 torch.tensor(x.reshape(-1, 1).tolist()))[:, i], a, b, b * 2)
@@ -95,20 +95,20 @@ class LBA:
 
 
 if __name__ == "__main__":
-    A = torch.tensor([3.0], requires_grad=True)
-    b = torch.tensor([10.0], requires_grad=True)
-    d = torch.tensor([2.1, 4.3, 1.8], requires_grad=True)
-    s = torch.tensor([1.0], requires_grad=True)
+    A = torch.tensor(3.0, requires_grad=True)
+    b = torch.tensor(10.0, requires_grad=True)
+    d = torch.tensor([2.1, 2.14, 2.18], requires_grad=True)
+    s = torch.tensor(1.0, requires_grad=True)
 
     lba = LBA(A, b, d, s)
     upper = b.item() / d.max().item() * 4
     t = np.linspace(0, upper, 1000).reshape((-1, 1))
 
-    p = lba.probs()
-    print('Analytical: ', p.detach().numpy())
+    p = lba.probs().detach().numpy()
+    print('Analytical: ', p/p.sum())
 
     rt, resp = sample_lba(100000, b.item(), A.item(),
-                          d.detach().reshape(1, -1), torch.ones((1, 3)), 0)
+                          d.detach().reshape(1, -1), torch.ones((1, 3)) * s.item(), 0)
     resp_freq, _ = np.histogram(resp, 3)
     print('Emprical: ', resp_freq/100000)
 
